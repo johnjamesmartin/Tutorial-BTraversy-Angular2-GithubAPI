d9822063854a631d873f2d5d2b9d4fc5ef77490a
“John <johnjamesmartin@live.com>
2016-10-26 00:37:35 +0100

    Github component and services drafted

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent }  from './app.component';
+import { GithubComponent }  from './components/github.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports: [ BrowserModule, HttpModule ],
+  declarations: [ AppComponent, GithubComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
---
+++ app/services/github.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+
+export class GithubService {
+    private username = 'johnjamesmartin';
+    private client_id = 'df9ddd55462355284a6f';
+    private client_secret = '62efba979c8979bc511b619ceb2625e9b4628d6e';
+
+    constructor(private _http:Http) {
+        console.log('Github service init...');
+    }
+}
